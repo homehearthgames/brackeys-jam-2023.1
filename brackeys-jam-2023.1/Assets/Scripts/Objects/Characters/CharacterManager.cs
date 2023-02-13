f1455d772caa50e6a6769d67f84193b82f3795b8
Nanqi <fafalnq@gmail.com>
2023-02-13 16:11:43 +0800

    Basic Character Controller
    Character Control features:
      - Move
      - Jump (With fixed height)

---
+++ brackeys-jam-2023.1/Assets/Scripts/Objects/Characters/CharacterManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterManager : MonoBehaviour
+{
+    public static CharacterManager instance;
+    
+    void Awake()
+    {
+        if(instance != null && instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public static float CalculateJumpForce(float gravityStrength, float jumpHeight)
+    {
+        //h = v^2/2g
+        //2gh = v^2
+        //sqrt(2gh) = v
+        return Mathf.Sqrt(2 * gravityStrength * jumpHeight);
+    }
+}
---
+++ brackeys-jam-2023.1/Assets/Scripts/Objects/Characters/PlayerController.cs
@@ -0,0 +1,134 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    #region Variables
+    [SerializeField] private Rigidbody2D _rb;
+
+    [Header("Speed Setting")]
+    [SerializeField] private float _speed = 5f;
+    private Vector2 inputDirection;
+
+    [Header("Jump Settings")]
+    // Jump related
+    // [SerializeField] private float _minJumpHeight = 1.5f;
+    [SerializeField] private float _maxJumpHeight = 2f;
+    private float minJumpForce;
+    private float maxJumpForce;
+
+    private bool jumpButtonDown;
+    private bool jumpButtonUp;
+    private bool isJumping = false;
+    private bool isGrounded = false;
+    [SerializeField] private Transform feetPos;
+    [SerializeField] private float checkRadius;
+    [SerializeField] private LayerMask whatIsGround;
+
+    // Status related
+    public enum Status
+    {
+        me,
+        soul,
+        dead
+    };
+    [Header("Status Settings")]
+    [SerializeField] private Status _status;
+
+    #endregion
+
+    #region Main Methods
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Calculate jump force
+        maxJumpForce = CharacterManager.CalculateJumpForce(Physics2D.gravity.magnitude * _rb.gravityScale, _maxJumpHeight + 0.25f);
+        Debug.Log(maxJumpForce);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GatherInput();
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+        Jump();
+    }
+    #endregion
+
+    #region Helper Methods
+    private void GatherInput()
+    {
+        isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
+
+        // Horizontal moevment
+        // Get horizontal input direction and jump input from input manager
+        float x;
+        bool jumpButtonDown;
+        bool jumpButtonUp;
+
+        switch (_status)
+        {
+            case Status.me:
+                // When the character is Me
+                // Get Axis from "A" and "D"
+                x = Input.GetAxisRaw("Horizontal");
+                jumpButtonDown = Input.GetButtonDown("MeJump");
+                jumpButtonUp = Input.GetButtonUp("MeJump") || Input.GetButtonUp("SoulJump");
+                break;
+            case Status.soul:
+                // When the character is Soul
+                // Get Axis from "Left" and "Right"
+                x = Input.GetAxisRaw("Horizontal");
+                jumpButtonDown = Input.GetButtonDown("SoulJump");
+                jumpButtonUp = Input.GetButtonUp("MeJump") || Input.GetButtonUp("SoulJump");
+                break;
+            default:
+                // When the character is dead
+                x = 0;
+                jumpButtonDown = false;
+                jumpButtonUp = false;
+                break;
+        }
+        
+        // Horizontal movement
+        inputDirection = new Vector2(x, 0);
+        if(inputDirection.sqrMagnitude > 1)
+        {
+            inputDirection.Normalize();
+        }
+        
+        // Jump
+        // Since holding down jump button jumps higher, this 
+        if(jumpButtonDown && isGrounded)
+        {
+            isJumping = true;
+            _rb.AddForce(Vector2.up * maxJumpForce * _rb.mass, ForceMode2D.Impulse);
+            // _rb.velocity = Vector2.up * maxJumpForce;
+        }
+        if(jumpButtonUp)
+        {
+            isJumping = false;
+            if(_rb.velocity.y > 0)
+            {
+                _rb.velocity = Vector2.zero;
+            }
+        }
+    }
+
+    private void Move()
+    {
+        _rb.velocity = new Vector2(inputDirection.x * _speed, _rb.velocity.y);
+    }
+
+    private void Jump()
+    {
+        
+    }
+    #endregion
+}

c3239cc308468962128fd6a61cacc5af69624f59
Harry Cai <yh.harryc@hotmail.com>
2023-02-13 11:51:02 -0500

    Added direction and force control for instantiator
    added prefab for the debug objects

--- brackeys-jam-2023.1/Assets/Scripts/Debug/DebugInstantiator.cs
+++ brackeys-jam-2023.1/Assets/Scripts/Debug/DebugInstantiator.cs
@@ -6,10 +6,23 @@ public class DebugInstantiator : MonoBehaviour
 {
     [SerializeField] private float spawnInterval=2f;
     [SerializeField] private GameObject spawnedObject;
+    [SerializeField] private int maxSpawns = 1;
+    private Queue<GameObject> _spawnedObjectQueue= new Queue<GameObject>();
+    [SerializeField, Range(-1.0f, 1.0f)]
+    private float x = 0f;
+    [SerializeField, Range(-1.0f, 1.0f)]
+    private float y = 1f;
+    public Vector2 ForceDirection{
+        get{return new Vector2(x,y);}
+    }
+    [SerializeField, Range(0.0f, 20.0f)]
+    private float forceOnSpawn = 10;
     // Start is called before the first frame update
+
     void Start()
     {
-         StartCoroutine(SpawnPrefabCoroutine());
+        SpawnPrefab();
+        StartCoroutine(SpawnPrefabCoroutine());
     }
 
     // Update is called once per frame
@@ -23,7 +36,21 @@ public class DebugInstantiator : MonoBehaviour
         while (true)
         {
             yield return new WaitForSeconds(spawnInterval);
-            Instantiate(spawnedObject, transform.position, Quaternion.identity);
+            
+            SpawnPrefab();
+            if (_spawnedObjectQueue.Count>maxSpawns){
+                Destroy(_spawnedObjectQueue.Dequeue());
+            }
+            
+        }
+    }
+
+    private void SpawnPrefab(){
+        GameObject instance = Instantiate(spawnedObject, transform.position, Quaternion.identity);
+        _spawnedObjectQueue.Enqueue(instance);
+        Rigidbody2D _rb = instance.GetComponent<Rigidbody2D>();
+        if(_rb!=null){
+            _rb.AddForce(this.ForceDirection.normalized * forceOnSpawn * _rb.mass, ForceMode2D.Impulse);
         }
     }
 }

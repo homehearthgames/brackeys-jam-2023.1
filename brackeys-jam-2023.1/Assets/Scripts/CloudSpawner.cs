4c92667c4013046b383a8c64ed70353d47a4c624
Nanqi <fafalnq@gmail.com>
2023-02-19 14:48:16 +0800

    Fixed cloud spawner and main menu cloud height

--- brackeys-jam-2023.1/Assets/Scripts/CloudSpawner.cs
+++ brackeys-jam-2023.1/Assets/Scripts/CloudSpawner.cs
@@ -14,20 +14,17 @@ public class CloudSpawner : MonoBehaviour {
 
     IEnumerator Start () {
         while (true) {
-            float randomY = Random.Range(-spawnHeight, spawnHeight);
+            float randomY;
             if (spawnOnTop) {
-                randomY = Random.Range(0f, 15);
+                randomY = Random.Range(0f, spawnHeight);
             } else {
                 randomY = Random.Range(-spawnHeight, 0f);
             }
             int randomIndex = Random.Range(0, cloudPrefabs.Length);
             GameObject cloudPrefab = cloudPrefabs[randomIndex];
             Vector3 spawnPos;
-            if (moveRight) {
-                spawnPos = new Vector3(-spawnHeight, randomY, 0f);
-            } else {
-                spawnPos = new Vector3(spawnHeight, randomY, 0f);
-            }
+            spawnPos = new Vector3(transform.position.x, randomY, 0f);
+            
             GameObject newCloud = Instantiate(cloudPrefab, spawnPos, Quaternion.identity);
             CloudMovement cloudMovement = newCloud.GetComponent<CloudMovement>();
             if (cloudMovement) {
